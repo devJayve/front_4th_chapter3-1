b15a18524667b8d30ae9f25e9cf1b63876799ba7
DevJayve <wndud4576@naver.com>
2025-02-03 16:23:05 +0900

    feat: msw  API 핸들러 설정

--- src/__mocks__/handlers.ts
+++ src/__mocks__/handlers.ts
@@ -1,15 +1,57 @@
 import { http, HttpResponse } from 'msw';
-import { Event } from '../types';
-import { events } from './response/events.json' assert { type: 'json' };
+
+import { Event, EventForm } from '../types';
+import { events as initialEvents } from './response/events.json' assert { type: 'json' };
+
+let events: Event[] = initialEvents as Event[];
 
 // ! HARD
 // ! 각 응답에 대한 MSW 핸들러를 작성해주세요. GET 요청은 이미 작성되어 있는 events json을 활용해주세요.
 export const handlers = [
-  http.get('/api/events', () => {}),
+  // 이벤트 리스트 조회
+  http.get('/api/events', () => {
+    console.log(events);
+    return HttpResponse.json({ events });
+  }),
 
-  http.post('/api/events', async ({ request }) => {}),
+  // 이벤트 추가
+  http.post('/api/events', async ({ request }) => {
+    const eventForm = (await request.json()) as EventForm;
+    const newEvent: Event = {
+      id: crypto.randomUUID(),
+      ...eventForm,
+    };
 
-  http.put('/api/events/:id', async () => {}),
+    events = [...events, newEvent];
 
-  http.delete('/api/events/:id', ({ params }) => {}),
+    return HttpResponse.json(newEvent, { status: 201 });
+  }),
+
+  // 이벤트 수정
+  http.put('/api/events/:id', async ({ params, request }) => {
+    const id = params.id;
+    const updateData = (await request.json()) as Partial<EventForm>;
+    const eventIndex = events.findIndex((event) => event.id === id);
+    if (eventIndex === -1) {
+      return new HttpResponse(null, { status: 404 });
+    }
+
+    const updatedEvent: Event = {
+      ...events[eventIndex],
+      ...updateData,
+    };
+
+    events = events.map((event) => (event.id === id ? updatedEvent : event));
+
+    return HttpResponse.json(updatedEvent);
+  }),
+
+  // 이벤트 삭제
+  http.delete('/api/events/:id', ({ params }) => {
+    const id = params.id;
+
+    events = events.filter((event) => event.id !== id);
+
+    return new HttpResponse(null, { status: 204 });
+  }),
 ];
